71d1bedb68d9a075b741d01473e03f2e46762606
rookieraa <zaaraalzh@gmail.com>
2024-05-05 23:54:42 +0700

    Menambahkan dan Implementasi dari Aljabar Library

---
+++ AljabarApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Aljabar;
+
+namespace AljabarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan koefisien a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan koefisien b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan koefisien c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double[] koefisien = { a, b, c };
+
+            double[] akar = Aljabar.Aljabar.AkarPersamaanKuadrat(koefisien);
+
+            if (akar != null)
+            {
+                Console.WriteLine("Akar persamaan kuadrat:");
+                foreach (double root in akar)
+                {
+                    Console.WriteLine(root);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Akar persamaan kuadrat adalah imajiner.");
+            }
+        }
+    }
+}
